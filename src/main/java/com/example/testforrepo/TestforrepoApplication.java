b4c0afbc02839c4681c2bda2fb26931ff7f674ed
Joe NOei <limingw4159@gmail.com>
2021-07-31 22:12:25 +1000

    first initlizations

---
+++ src/main/java/com/example/testforrepo/TestforrepoApplication.java
@@ -0,0 +1,13 @@
+package com.example.testforrepo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestforrepoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestforrepoApplication.class, args);
+    }
+
+}
